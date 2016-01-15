c0068c4c318fe936070b1fb99de71cb092236c20
Ramiro Rojo <ramiro.rojo.cretta@gmail.com>
2016-01-15 14:05:16 -0300

    Update package.js
    Moved for latest version of stylus to allow meteor packages

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package.describe({
 Package.on_use(function (api) {
 	api.use(['session@1.0.0',
 		'spacebars@1.0.0',
-		'stylus@1.0.0',
+		'stylus@2.511.0',
 		'accounts-base@1.0.0',
 		'underscore@1.0.0',
 		'templating@1.0.0'
